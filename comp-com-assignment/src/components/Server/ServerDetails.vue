c35a138aa4efb02419e23fb299fe436f24859f34
Sarah Harrington <sarah.soberg@gmail.com>
2019-04-03 17:13:38 -0500

    updated per assignment video

--- comp-com-assignment/src/components/Server/ServerDetails.vue
+++ comp-com-assignment/src/components/Server/ServerDetails.vue
@@ -1,13 +1,35 @@
 <template>
     <div class="col-xs-12 col-sm-6">
-        <p>Server Details are currently not updated</p>
+        <p v-if="!server">Please select a server.</p>
+        <p v-else>Server selected: {{ server.id }} Current Status: {{ server.status }}</p>
+        <hr>
+        <button @click="resetStatus">Change to Normal</button>
     </div>
 
 </template>
 
 <script>
-</script>
+    import { serverBus } from '../../main';
 
+    export default {
+        data: function() {
+            return {
+                server: null
+            }
+        },
+        methods: {
+            resetStatus() {
+                this.server.status = 'normal'
+            }
+        },
+        created() {
+            serverBus.$on('serverSelected', (server) => {
+                this.server = server;
+            });
+        }
+    }
+</script>
+    
 <style>
 
 </style>
