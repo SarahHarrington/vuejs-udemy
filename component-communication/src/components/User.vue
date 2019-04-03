da82490a3d813c471e3dbc427c4bc531b798d6bf
Sarah Harrington <sarah.soberg@gmail.com>
2019-04-03 14:38:15 -0500

    lots of comments/notes through lecture

--- component-communication/src/components/User.vue
+++ component-communication/src/components/User.vue
@@ -2,10 +2,17 @@
     <div class="component">
         <h1>The User Component</h1>
         <p>I'm an awesome User!</p>
+        <button @click="changeName">Change my Name</button>
+        <p>Name is {{ name }}</p>
         <hr>
         <div class="row">
             <div class="col-xs-12 col-sm-6">
-                <app-user-detail></app-user-detail>
+                <!-- Use the colon here to bind to the name attribute below -->
+                <!-- Otherwise, it just interprets it as a string -->
+                <!-- Also needs to match the prop in the array in the child component -->
+                <app-user-detail :name="name" @nameWasReset="name = $event"></app-user-detail>
+                <!-- The @nameWasReset is like v-on -->
+                <!-- $event is the event that was sent with the emit event -->
             </div>
             <div class="col-xs-12 col-sm-6">
                 <app-user-edit></app-user-edit>
@@ -19,9 +26,19 @@
     import UserEdit from './UserEdit.vue';
 
     export default {
+        data: function() {
+            return {
+                name: 'Sarah'
+            }
+        },
         components: {
             appUserDetail: UserDetail,
             appUserEdit: UserEdit
+        }, 
+        methods: {
+            changeName() {
+                this.name = "Blorg"
+            }
         }
     }
 </script>
--- component-communication/src/components/UserDetail.vue
+++ component-communication/src/components/UserDetail.vue
@@ -2,10 +2,50 @@
     <div class="component">
         <h3>You may view the User Details here</h3>
         <p>Many Details</p>
+        <!-- The {{ name }} needs to match the prop below-->
+        <p>User Name: {{ switchName() }}</p>
+        <button @click="resetName">Reset Name</button>
     </div>
 </template>
 
 <script>
+    export default {
+        //the name here has to match the property we're using in the template
+        // You can also use case sensative like myName - this only works in
+        // a single file template
+        props: {
+            //By making the prop an object, you can add validation
+            //Type is the type of prop youre expecting
+            //You can also use required or default
+            //If you use 
+            // type: Object,
+            // default: function() {
+                // return {
+                    //name: 'Max'
+                //}
+            //}
+            name: {
+                type: String,
+                required: true
+            }
+        },
+        methods: {
+            //a prop can be used like any other property in the whole vue object
+            switchName() {
+                return this.name.split("").reverse().join('')
+            },
+            resetName() {
+                //If your property is an array or object, it only exists in memory once
+                //if you change it like this, it would also change it in memory
+                //Passes a pointer
+                //Strings passes a copy
+                //This string, only changes it in the child, to change it in the parent
+                //we need to create a custom event
+                this.name = 'Boom Shaka Laka';
+                this.$emit('nameWasReset', this.name);
+            }
+        }
+    }
 </script>
 
 <style scoped>
