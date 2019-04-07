c34ce92e834bb4d0de0527b8f5598e8e2a344620
Sarah Harrington <sarah.soberg@gmail.com>
2019-04-07 00:14:23 -0500

    finished lecture

--- forms/src/App.vue
+++ forms/src/App.vue
@@ -13,6 +13,10 @@
                                 class="form-control"
                                 v-model="userData.email">
                     </div>
+                    <!-- v-model binds the value of a the input
+                    example:
+                    :value="userData.email"
+                    @input="userData.email === $event.target.value" -->
                     <div class="form-group">
                         <label for="password">Password</label>
                         <input
@@ -108,17 +112,23 @@
                     </select>
                 </div>
             </div>
+            <div class="row">
+                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                    <app-switch v-model="dataSwitch"></app-switch>
+                </div>
+            </div>
             <hr>
             <div class="row">
                 <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                     <button
-                            class="btn btn-primary">Submit!
+                            class="btn btn-primary"
+                            @click.prevent="submitted">Submit!
                     </button>
                 </div>
             </div>
         </form>
         <hr>
-        <div class="row">
+        <div class="row" v-if="isSubmitted">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <div class="panel panel-default">
                     <div class="panel-heading">
@@ -137,7 +147,7 @@
                         </ul>
                         <p>Gender: {{ gender }} </p>
                         <p>Priority: {{ selectedPriority }} </p>
-                        <p>Switched:</p>
+                        <p>Switched: {{ dataSwitch }}</p>
                     </div>
                 </div>
             </div>
@@ -146,6 +156,8 @@
 </template>
 
 <script>
+    import Switch from './Switch.vue';
+
     export default {
         data() {
             return {
@@ -158,8 +170,18 @@
                 sendMail: [],
                 gender: 'none',
                 priorities: ['High', 'Medium', 'Low'],
-                selectedPriority: 'High'
+                selectedPriority: 'High',
+                dataSwitch: true,
+                isSubmitted: false
             }
+        },
+        methods: {
+            submitted() {
+                this.isSubmitted = true;
+            }
+        },
+        components: {
+            appSwitch: Switch
         }
     }
 </script>
