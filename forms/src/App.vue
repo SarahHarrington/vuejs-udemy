0459cf803c715a48c369e6f6d22864249e4ba197
Sarah Harrington <sarah.soberg@gmail.com>
2019-04-06 23:49:31 -0500

    updated based on lecture

--- forms/src/App.vue
+++ forms/src/App.vue
@@ -10,21 +10,28 @@
                         <input
                                 type="text"
                                 id="email"
-                                class="form-control">
+                                class="form-control"
+                                v-model="userData.email">
                     </div>
                     <div class="form-group">
                         <label for="password">Password</label>
                         <input
                                 type="password"
                                 id="password"
-                                class="form-control">
+                                class="form-control"
+                                v-model.lazy="userData.password">
                     </div>
+                    <!-- the .lazy modifier changes it so it doesn't update the 
+                    binding until after the focus has left that input
+                    Otherwise, it goes after every keystroke 
+                    .number is another modifier-->
                     <div class="form-group">
                         <label for="age">Age</label>
                         <input
                                 type="number"
                                 id="age"
-                                class="form-control">
+                                class="form-control"
+                                v-model="userData.age">
                     </div>
 
                 </div>
@@ -33,26 +40,33 @@
                 <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 form-group">
                     <label for="message">Message</label><br>
                     <!-- Interpolation between <textarea>{{ test }}</textarea> doesn't work!-->
+                    <!-- The above not applies to if you want a default message -->
                     <textarea
                             id="message"
                             rows="5"
-                            class="form-control"></textarea>
+                            class="form-control"
+                            v-model="message"></textarea>
                 </div>
             </div>
             <div class="row">
                 <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                     <div class="form-group">
                         <label for="sendmail">
+                            <!-- We assign the same model to both inputs
+                            Vue knows to take the values of both inputs
+                            and combine them in the array-->
                             <input
                                     type="checkbox"
                                     id="sendmail"
-                                    value="SendMail"> Send Mail
+                                    value="SendMail"
+                                    v-model="sendMail"> Send Mail
                         </label>
                         <label for="sendInfomail">
                             <input
                                     type="checkbox"
                                     id="sendInfomail"
-                                    value="SendInfoMail"> Send Infomail
+                                    value="SendInfoMail"
+                                    v-model="sendMail"> Send Infomail
                         </label>
                     </div>
 
@@ -61,16 +75,22 @@
             <div class="row">
                 <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 form-group">
                     <label for="male">
+                        <!-- By binding to the same model, vue knows that
+                        the radio inputs should be grouped together allowing
+                        only one to be selected
+                        It also saves the value of the input to the model -->
                         <input
                                 type="radio"
                                 id="male"
-                                value="Male"> Male
+                                value="Male"
+                                v-model="gender"> Male
                     </label>
                     <label for="female">
                         <input
                                 type="radio"
                                 id="female"
-                                value="Female"> Female
+                                value="Female"
+                                v-model="gender"> Female
                     </label>
                 </div>
             </div>
@@ -79,8 +99,12 @@
                     <label for="priority">Priority</label>
                     <select
                             id="priority"
-                            class="form-control">
-                        <option></option>
+                            class="form-control"
+                            v-model="selectedPriority">
+                        <option 
+                            v-for="priority in priorities">
+                            {{ priority }}
+                        </option>
                     </select>
                 </div>
             </div>
@@ -101,16 +125,18 @@
                         <h4>Your Data</h4>
                     </div>
                     <div class="panel-body">
-                        <p>Mail:</p>
-                        <p>Password:</p>
-                        <p>Age:</p>
-                        <p>Message: </p>
+                        <p>Mail: {{ userData.email }}</p>
+                        <p>Password: {{ userData.password }}}</p>
+                        <p>Age: {{ userData.age }} </p>
+                        <!-- This style tag will keep the formatting w/
+                        spacing, this is CSS, not Vue specific -->
+                        <p style="white-space: pre">Message: {{ message }}</p>
                         <p><strong>Send Mail?</strong></p>
                         <ul>
-                            <li></li>
+                            <li v-for="item in sendMail">{{ item}}</li>
                         </ul>
-                        <p>Gender:</p>
-                        <p>Priority:</p>
+                        <p>Gender: {{ gender }} </p>
+                        <p>Priority: {{ selectedPriority }} </p>
                         <p>Switched:</p>
                     </div>
                 </div>
@@ -121,6 +147,20 @@
 
 <script>
     export default {
+        data() {
+            return {
+                userData: {
+                    email: '',
+                    password: '',
+                    age: 36
+                },
+                message: 'A new text',
+                sendMail: [],
+                gender: 'none',
+                priorities: ['High', 'Medium', 'Low'],
+                selectedPriority: 'High'
+            }
+        }
     }
 </script>
 
