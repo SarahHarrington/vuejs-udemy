3ffc85114123e6d7ad98b8f3cef7554b989759dd
Sarah Harrington <sarah.soberg@gmail.com>
2019-04-07 21:05:45 -0500

    updated per lecture with notes

--- directives/src/App.vue
+++ directives/src/App.vue
@@ -1,19 +1,65 @@
 <template>
-    <div class="container">
-        <div class="row">
-            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                <h1>Directives</h1>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <h1>Directives</h1>
+        <!-- directives are the v- that you put on elements -->
 
-            </div>
-        </div>
+        <!-- These are built in directives -->
+        <p v-text="'Some Text'"></p>
+        <p v-html="'More text'"></p>
+      </div>
     </div>
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <h1>Custom Directives</h1>
+        <!-- Addinging the :background is adding an arg to the directive -->
+        <!-- the modifier can be added and then defined in the directive -->
+        <p v-highlight:background.delayed="'purple'">Color This</p>
+        <p v-local-highlight:background.delayed.blink="{mainColor: 'red', secondColor:'blue', delay: 500}">Another Color</p>
+      </div>
+    </div>
+    
+  </div>
 </template>
 
 <script>
-    export default {
+export default {
+  directives: {
+    "local-highlight": {
+      bind(el, binding, vnode) {
+        var delay = 0;
+        if (binding.modifiers["delayed"]) {
+          delay = 3000;
+        }
+        if (binding.modifiers["blink"]) {
+          let mainColor = binding.value.mainColor;
+          let secondColor = binding.value.secondColor;
+          let currentColor = mainColor;
+          setTimeout(() => {
+            setInterval(() => {
+              currentColor == secondColor ? currentColor = mainColor : currentColor = secondColor;
+              if (binding.arg === "background") {
+                el.style.backgroundColor = currentColor;
+              } else {
+                el.style.color = currentColor;
+              }
+            }, binding.value.delay);
+          }, delay);
+        } else {
+          setTimeout(() => {
+          if (binding.arg === "background") {
+            el.style.backgroundColor = binding.value.mainColor;
+          } else {
+            el.style.color = binding.value.mainColor;
+          }
+        }, delay);
+        }
+      }
     }
+  }
+};
 </script>
 
 <style>
-
 </style>
--- directives/src/main.js
+++ directives/src/main.js
@@ -1,6 +1,48 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+Vue.directive('highlight', {
+  // 5methods avaialble as hooks
+  // bind - as soon as the directive is bound to the element
+  // bind(el, binding, vnode)
+  //    the element, the binding, the node in the virtual dom
+
+  // inserted - as soon as the element is in the parent node
+  // insert(el, binding, vnode)
+
+  // update - once the component is updated w/out children
+  // update(el, binding, vnode, oldeVnode)
+
+  // componentUpdated - once component is updated w/children
+  // componentUpdated(el, binding, vnode, oldVnode)
+
+  // unbind - once the directive is removed
+  // unbind(el, binding, vnode)
+
+  bind(el, binding, vnode) {
+    // el.style.backgroundColor = 'green';
+
+    // In this ex., the .value grabs the value betweent the quotes
+    // el.style.backgroundColor = binding.value;
+
+    //the .arg let's us access the argument passed on the
+    // v-highlight:background
+
+    var delay = 0;
+    if (binding.modifiers['delayed']) {
+      delay = 3000;
+    }
+    setTimeout(() => {
+      if (binding.arg === 'background') {
+        el.style.backgroundColor = binding.value;
+      } else {
+        el.style.color = binding.value;
+      }
+    }, delay)
+
+  }
+}); 
+
 new Vue({
   el: '#app',
   render: h => h(App)
