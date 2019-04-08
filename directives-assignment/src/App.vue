2031e2a23a13edd4ebf6a84e025ddbfa98303167
Sarah Harrington <sarah.soberg@gmail.com>
2019-04-07 21:31:19 -0500

    followed along with assignment video

--- directives-assignment/src/App.vue
+++ directives-assignment/src/App.vue
@@ -3,16 +3,45 @@
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Directives Exercise</h1>
+                <!-- v-myon:click="only do functions to execute" -->
                 <!-- Exercise -->
                 <!-- Build a Custom Directive which works like v-on (Listen for Events) -->
-
+                <button v-customOn:click="clicked" class="btn btn-primary">Click Me</button>
+                <div 
+                    style="width: 100px; height: 100px; background-color: purple;"
+                    v-customOn:mouseenter="mouseenter"
+                    v-customOn:mouseleave="mouseleave"></div>
             </div>
         </div>
     </div>
 </template>
 
 <script>
-    export default {}
+    export default {
+        directives: {
+            customOn: {
+                bind(el, binding) {
+                    // el.onclick = function() {
+                    //     binding.value();
+                    // }
+                    const type = binding.arg;
+                    const fn = binding.value;
+                    el.addEventListener(type, fn);
+                }
+            }
+        }, 
+        methods: {
+            clicked() {
+                alert('I was clicked')
+            },
+            mouseenter() {
+                console.log('mouse entered');
+            },
+            mouseleave() {
+                console.log('mouse left')
+            }
+        }
+    }
 </script>
 
 <style>
